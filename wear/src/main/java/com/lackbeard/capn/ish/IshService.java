108e12157f87d028ad454db8dfd8b5480e4bdf57
Dan <daniel.belsey@bt.com>
2015-04-26 17:10:36 +0100

    fix gitignore, also get all the stuff auto-generated by github (I totally understand how rebasing works)

---
+++ wear/src/main/java/com/lackbeard/capn/ish/IshService.java
@@ -0,0 +1,9 @@
+package com.lackbeard.capn.ish;
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
+/**
+ * Created by Dan on 26/04/2015.
+ */
+public class IshService extends CanvasWatchFaceService {
+}
